7e6b240787788a2514585977707f01d67b766580
Jatin Suneja <jatinsuneja1903@gmail.com>
2018-06-12 17:09:39 -0700

    basic database models

--- ingredients/models.py
+++ ingredients/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length = 100)
+
+    def ___str__(self):
+        return self.name
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=100)
+    notes = models.TextField()
+    category = models.ForeignKey(
+        Category, related_name = 'Ingredients', on_delete= models.CASCADE
+    )
+
+    def __str__(self):
+        return self.name
